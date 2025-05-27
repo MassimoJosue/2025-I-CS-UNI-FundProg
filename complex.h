b97a8d3aa0ce23ce3423132a7cc4724b3fe9e262
ecuadros <ecuadros@spc.org.pe>
2025-05-27 13:32:06 -0500

    operator += implemented as a first example

--- complex.h
+++ complex.h
@@ -4,20 +4,25 @@
 using RealType = double;
 using ImagType = double;
 
+// Complex
 class Complex
 {
 private:
     // Real part
-    // RealType m_r = 0;
+    RealType m_r = 0;
     // Real part
-    // ImagType m_i = 0;
+    ImagType m_i = 0;
 public:
     // Constructores
 
     // Destructor
 
     // Operadores
-    
+    Complex& operator+=(const Complex& other) {
+        m_r += other.getReal();
+        m_i += other.getImag();
+        return *this;
+    }
 };
 
 #endif // __COMPLEX_H__
