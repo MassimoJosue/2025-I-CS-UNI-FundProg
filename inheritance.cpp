9fe93def73e1c532e872eaeca41879252ce59e07
Diego Vasquez <diegoalonzovasquezmorales165@gmail.com>
2025-06-09 18:16:14 -0500

    clase student

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "inheritance.h"
 #include "academico/professor.h"
+#include "academico/student.h"
 using namespace std;
 
 void DemoInheritance(){
     cout << "DemoInheritance" << endl;
 
     Professor p1 ("Ernesto", DocTypeDNI, "12345678", 100);
+
+    Student s1("Diego", DocTypeDNI, "12345678", "20202125C");
     
 }
